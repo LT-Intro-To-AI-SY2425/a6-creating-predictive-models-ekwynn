9972a10d167f4ac08561e2bbd20bcbda4ea31ba1
Ethan Wynn <ekwynn@cps.edu>
2024-12-19 10:41:47 -0600

    finished everything

--- part1-linear-regression/example1_checking_correlation.py
+++ part1-linear-regression/example1_checking_correlation.py
@@ -10,7 +10,8 @@ y = data["Chirps"]
 # sets the size of the graph
 plt.figure(figsize=(6,4))
 
-# creates a scatter plot and labels the axes
+# creates 
+#  scatter plot and labels the axes
 plt.scatter(x,y)
 plt.xlabel("Temperature °F")
 plt.ylabel("Chirps per Minute")
--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -9,14 +10,41 @@ x = data[["miles","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 
 #create linear regression model
+model = LinearRegression().fit(xtrain, ytrain)
 
-#Find and print the coefficients, intercept, and r squared values. 
-#Each should be rounded to two decimal places. 
+#Find and print the coefficients, intercept, and r squared values.
+#Each should be rounded to two decimal places.
+
+# get the coef_, intercept_ valuesm and r^2 values
+# use float() to turn the arrays into a single float value
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
+
+# print out the linear equation and r^2 value
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {coef[2]}x3 + {intercept}")
+print("R Squared value:", r_squared)
 
 
-#Loop through the data and print out the predicted prices and the 
+#Loop through the data and print out the predicted prices and the
 #actual prices
 print("***************")
 print("Testing Results")
+
+
+predict= model.predict(xtest)
+
+predict=np.around(predict,2)
+print(predict)
+
+print("\nTesting Multivariable Model with Testing Data:")
+for index in range(len(xtest)):
+    actual = ytest[index] # gets the actual y value from the y test dataset
+    predicted_y = predict[index] # gets the predicted y value from the predict variable
+    x_coord = xtest[index] # gets the x value from the x test dataset
+    print(f"Miles Driven: {x_coord[0]} Age: {x_coord[1]}Price: {x_coord[2]} Actual: {actual} Predicted: {predicted_y}")
+
+
